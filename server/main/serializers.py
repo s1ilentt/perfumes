3afa2d45176159ac088920b8b521bf3f44ea5d11
Oleksii <aleksejlitovcenko69@gmail.com>
2025-09-30 14:03:03 +0300

    post method

--- server/main/serializers.py
+++ server/main/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from .models import Perfume
+from .models import Perfume, Category
 
 
-class PerfumeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Perfume
-        fields = ('name', 'category')
+class PerfumeSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    description = serializers.CharField()
+    price = serializers.FloatField()
+    mark = serializers.FloatField()
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+
+
+
--- server/main/views.py
+++ server/main/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-from rest_framework import generics
-
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.forms.models import model_to_dict
 from .models import Perfume
 from .serializers import PerfumeSerializer
+from rest_framework.views import APIView
+from rest_framework import generics
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status  # Для использования HTTP-кодов
 
 
-class PerfumeList( generics.ListAPIView):
-    queryset = Perfume.objects.all()
-    serializer_class = PerfumeSerializer
+class PerfumeList(APIView):
+    def get(self, request, *args, **kwargs):
+        p = Perfume.objects.all()
+        return Response({'perfumes': PerfumeSerializer(p, many=True).data})
+
+    def post(self, request, *args, **kwargs):
+        serializer = PerfumeSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            validated_data = serializer.validated_data
+            post_new = Perfume.objects.create(**validated_data)
+
+            return Response({'perfume': PerfumeSerializer(post_new).data},
+                            status=status.HTTP_201_CREATED)
+
+
+
+
+
+
+
+
