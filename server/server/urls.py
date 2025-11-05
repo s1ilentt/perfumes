f432ce2d24391521dd6911510c39ea59c5951e69
Oleksii <aleksejlitovcenko69@gmail.com>
2025-11-05 15:36:15 +0200

    category view

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,24 +1,9 @@
-"""
-URL configuration for server project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
-from main.views import CartViewSet, CartItemViewSet, PerfumeList, PerfumeDetail
+from main.views import CartViewSet, CartItemViewSet, PerfumeList, PerfumeDetail, CategoryList
 from django.conf import settings
 
 router = DefaultRouter()
@@ -28,6 +13,7 @@ router.register(r'cart-items', CartItemViewSet, basename='cart-items')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/perfumes/', PerfumeList.as_view(), name='perfume-list'),
+    path('api/v1/categories/', CategoryList.as_view(), name='category-list'),
     path('api/v1/', include(router.urls)),
     path("api/v1/perfumes/<int:pk>/", PerfumeDetail.as_view(), name="perfume-detail"),
 ]
